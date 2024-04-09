4d17eee5b72399a7d4959624d2160741224ea3ff
“itNapominki” <it.napominki.mos@gmail.com>
2024-04-09 10:58:50 +0300

    открыл пароль бд

--- src/core/dataBase/dataBase.js
+++ src/core/dataBase/dataBase.js
@@ -11,9 +11,8 @@ class DataBase {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
-      // password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,      
     });
     
     return connection;
