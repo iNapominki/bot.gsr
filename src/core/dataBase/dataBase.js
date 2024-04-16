55bcd4ce1fca894f0fa10cc8214cbd3f0523f271
“itNapominki” <it.napominki.mos@gmail.com>
2024-04-16 13:16:20 +0300

    fix

--- src/core/dataBase/dataBase.js
+++ src/core/dataBase/dataBase.js
@@ -11,7 +11,7 @@ class DataBase {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
-      //password: process.env.DB_PASSWORD,
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,      
     });
     
