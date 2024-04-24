3f187bb09d1b86bb2c25ffa7560f62e69d3926b0
“itNapominki” <it.napominki.mos@gmail.com>
2024-04-24 14:10:27 +0300

    установил сервер express, добавил описание при отказе

--- src/bot/Bot.js
+++ src/bot/Bot.js
@@ -6,13 +6,62 @@ const AipUse = require("../utils/api/apiUse");
 const Api = require("../utils/api/api");
 const SessionRegistration = require("../session/session.registration");
 const cacheService = require("../cache/CacheService");
+const express = require('express')
+const port = process.env.EXPRESS_PORT || 8000;
 
 class Bot {
+  /**
+   * @param {string} token 
+   */
   constructor(token) {
     this.bot = new TelegramBot(token, { polling: true });
+
+     const app = express();
+     app.use(express.json());
+     app.get('/', function (req, res) {
+      res.send({status: true});
+    })
+    
+      // Запускаем Express сервер
+    app.listen(port, () => {
+      console.log(`Express server слушает порт ${port}`);
+    });
   }
 
-    //быстрая проверка заполнено ли имя у пользователя телеграмм
+  // constructor(token) {
+  //   const TOKEN = token;
+  //   const url = 'http://95.181.226.126';
+  //   //const port = process.env.EXPRESS_PORT || 3000;
+  //   const port = 8000;
+
+  //   this.bot = new TelegramBot(TOKEN);
+
+  //   // Устанавливаем webhook
+  //   this.bot.setWebHook(`${url}/bot${TOKEN}`);
+
+  //   const app = express();
+    
+  //   // Парсим обновления в формат JSON
+  //   app.use(express.json());
+
+  //   // Обрабатываем обновления от Telegram
+  //   app.post(`/bot${TOKEN}`, (req, res) => {
+  //     this.bot.processUpdate(req.body);
+  //     res.sendStatus(200);
+  //   });
+
+  //   // Запускаем Express сервер
+  //   app.listen(port, () => {
+  //     console.log(`Express server слушает порт  ${port}`);
+  //   });
+  
+  // }
+
+  //быстрая проверка заполнено ли имя у пользователя телеграмм
+  /**
+   * @param {number} chatId 
+   * @param {string | undefined} chatUsername 
+   */
     checkUserName(chatId, chatUsername) {
       let isCheckUserName = true;
       if (!chatUsername) {
