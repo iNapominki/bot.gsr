646859e6f514aabb42bf70ae3a15a997dcf0ffb8
“itNapominki” <it.napominki.mos@gmail.com>
2024-10-14 17:36:53 +0300

    вынес  env

--- src/utils/api/apiWeb.js
+++ src/utils/api/apiWeb.js
@@ -5,7 +5,7 @@ const GOOGLE_SHEETS_KEY_ORDERS = process.env.GOOGLE_SHEETS_KEY_ORDERS;
 const TELEGRAMM_ADMIN_CHAT = process.env.TELEGRAMM_ADMIN_CHAT;
 // const BOT_API_TOKEN = process.env.BOT_API_TOKEN;
 
-// const BOT_API_URL = process.env.BOT_API_URL;
+const BOT_API_URL = process.env.BOT_API_URL;
 const fetch = require("node-fetch");
 
 class ApiWeb {
@@ -56,77 +56,77 @@ class ApiWeb {
 
   // стартовая
   async botCommandStart(tlgId) {
-    let url = "http://devbot.e-feedback.ru/api/bot/command/start";
+    let url = BOT_API_URL + "/api/bot/command/start";
     const post = { tlgId: tlgId };
     this.postData(post, url);
   }
 
   // начать оформлять заявку
   async botCommandOrder(tlgId) {
-    let url = "http://devbot.e-feedback.ru/api/bot/command/order";
+    let url = BOT_API_URL + "/api/bot/command/order";
     const post = { tlgId: tlgId };
     this.postData(post, url);
   }
 
   // очистить оформление заявка и сбросить чаты
   async botCommandClear(tlgId) {
-    let url = "http://devbot.e-feedback.ru/api/bot/command/clear";
+    let url = BOT_API_URL + "/api/bot/command/clear";
     const post = { tlgId: tlgId };
     this.postData(post, url);
   }
 
   // последние 4 заказа получить кнопки
   async botCommandChats(tlgId) {
-    let url = "http://devbot.e-feedback.ru/api/bot/command/chats";
+    let url = BOT_API_URL + "/api/bot/command/chats";
     const post = { tlgId: tlgId };
     this.postData(post, url);
   }
 
   // войти в чат по номеру заказа
   async botButtonsChat(tlgId, numberOrder) {
-    let url = "http://devbot.e-feedback.ru/api/bot/buttons/chat";
+    let url = BOT_API_URL + "/api/bot/buttons/chat";
     const post = { tlgId: tlgId, numberOrder: numberOrder };
     this.postData(post, url);
   }
 
   // отправить вариант для заказа
   async botButtonsOrderVariant(tlgId, orderVariant) {
-    let url = "http://devbot.e-feedback.ru/api/bot/buttons/order/variant";
+    let url = BOT_API_URL + "/api/bot/buttons/order/variant";
     const post = { tlgId: tlgId, orderVariant: orderVariant };
     this.postData(post, url);
   }
 
   // уточнить быстрый статус
   async botButtonsOrderStatus(tlgId, numberOrder) {
-    let url = "http://devbot.e-feedback.ru/api/bot/buttons/order/status";
+    let url = BOT_API_URL + "/api/bot/buttons/order/status";
     const post = { tlgId: tlgId, numberOrder: numberOrder };
     this.postData(post, url);
   }
 
   // запросить другой вопрос по заказу
   async botButtonsOrderNumber(tlgId, numberOrder) {
-    let url = "http://devbot.e-feedback.ru/api/bot/buttons/order/number";
+    let url = BOT_API_URL + "/api/bot/buttons/order/number";
     const post = { tlgId: tlgId, numberOrder: numberOrder };
     this.postData(post, url);
   }
 
   // любое сообщение
   async botMessage(tlgId, message, deleMessage, messageId) {
-    let url = "http://devbot.e-feedback.ru/api/bot/message";
+    let url = BOT_API_URL + "/api/bot/message";
     const post = { tlgId: tlgId, message: message, deleMessage: deleMessage, messageId: messageId};
     this.postData(post, url);
   }
 
   // согласование сообщений
   async botButtonsApprove(tlgId, messageId, statusApprove ) {
-    let url = "http://devbot.e-feedback.ru/api/bot/buttons/approve";
+    let url = BOT_API_URL + "/api/bot/buttons/approve";
     const post = { tlgId: tlgId, messageId: messageId, statusApprove: statusApprove };
     this.postData(post, url);
   }
   // быстрая заявка, с авто заполнением некоторых шагов
      
      async botButtonsOrderFast(tlgId, pendingId) {
-      let url = "http://devbot.e-feedback.ru/api/bot/buttons/order/fast";
+      let url = BOT_API_URL + "/api/bot/buttons/order/fast";
       const post = { tlgId: tlgId, pendingId: pendingId };
       this.postData(post, url);
 }
