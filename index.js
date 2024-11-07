c8e35a2b29d4728310dac77d57dcce1052a91fe1
“itNapominki” <it.napominki.mos@gmail.com>
2024-11-07 11:38:02 +0300

    BN-dev-2-1: оптимизировал index.js

--- index.js
+++ index.js
@@ -10,11 +10,8 @@ const FormCommand = require("./src/commands/Form.command");
 const Command = require("./src/commands/command.class");
 const Buttoncommand = require("./src/commands/Button.command");
 const ClearOrderCommand = require("./src/commands/ClearOrder.command");
-const SessionServisPostOrder = require("./src/session/session.servisPostOrder");
-const DataBase = require("./src/core/dataBase/dataBase");
 const ChatsCommands = require("./src/commands/Chats.commands");
 const GetMyChatsCommand = require("./src/commands/GetMyChats.command");
-const LogOutChatsCommand = require("./src/commands/LogOutChats.command");
 const MyIdCommand = require("./src/commands/MyId.command");
 const CleanCacheCommand = require("./src/commands/CleanCache.command");
 const ContactCommand = require("./src/commands/Contact.command");
@@ -31,46 +28,24 @@ try {
   bot.start();
   const loggerManager = new LoggerManager();
 
-  const registration = new RegisrtationCommand(bot.bot);
-  const help = new HelpCommand(bot.bot);
-  const order = new OrderCommand(bot.bot);
-  const form = new FormCommand(bot.bot);
-  const button = new  Buttoncommand(bot.bot);
-  const clear = new ClearOrderCommand(bot.bot);
-  //const sessionServisPostOrder = new SessionServisPostOrder(bot.bot);
-  // подключаем базу данных
-  const batabase = new DataBase();
-  // подключаем чаты менеджер - агент - куратор
-  const chat = new ChatsCommands(bot.bot);
-  // получить мои чаты
-  const mychats = new GetMyChatsCommand(bot.bot);
-  // выход из чатов
-  const logoutChats = new LogOutChatsCommand(bot.bot);
-  // запросить мой id 
-  const myid = new MyIdCommand(bot.bot);
-  // инициализация кэша  
-  const cleancache = new CleanCacheCommand(bot.bot);
-  const contactcommand = new ContactCommand(bot.bot);
-  const photoCommand = new PhotoCommand(bot.bot);
+  // Создаем массив команд
+  const commands = [
+    new RegisrtationCommand(bot.bot),
+    new HelpCommand(bot.bot),
+    new OrderCommand(bot.bot),
+    new FormCommand(bot.bot),
+    new Buttoncommand(bot.bot),
+    new ClearOrderCommand(bot.bot),
+    new ChatsCommands(bot.bot),
+    new GetMyChatsCommand(bot.bot),
+    new MyIdCommand(bot.bot),
+    new CleanCacheCommand(bot.bot),
+    new ContactCommand(bot.bot),
+    new PhotoCommand(bot.bot)
+  ];
 
-  
-
-  help.handle();
-  registration.handle();
-  order.handle();
-  form.handle();
-  button.handle();
-  clear.handle();
-  // запускаем 1 раз сервис который будет по очереди отправлять запрос в Google Sheets
-  //sessionServisPostOrder.start();
-  //batabase.initial();
-  chat.handle();
-  mychats.handle();
-  //logoutChats.handle();
-  myid.handle();  
-  cleancache.handle();
-  contactcommand.handle();
-  photoCommand.handle();
+  // Инициализируем все команды
+  commands.forEach(command => command.handle());
   
   loggerManager.logMessage("log", "старт", "Произошел старт бота");
   // Пример обработки ошибки  
