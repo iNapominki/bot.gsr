fc99d30c50405697cf5821e6cc124d3e08e9fc10
“itNapominki” <it.napominki.mos@gmail.com>
2024-12-17 11:16:30 +0300

    Сброс поделиться номером, что бы не мешала

--- src/commands/Order.command.js
+++ src/commands/Order.command.js
@@ -41,50 +41,8 @@ class OrderCommand extends Command {
             },
           };
 
-
-
-      await this.bot.sendMessage(chatId, '.', options );  //Начато оформление заявки, заполняйте информацию по очереди
-     new ApiWeb(this.bot).botCommandOrder(chatId);
-      // проверка не нахожусь ли я в режиме чата
-      //await  new ChatHandle(this.bot).logoutChat(chatId);
-
-      // первичная проверка при создании заявки, заявку может создать только зарегистрированный пользователь
-      //this._useCheskUser(chatId).then((user) => {
-        
-        // if (user) {
-        //   // оформлять заявки только менеджер
-        //   if (!(typeof user == "string" || typeof user == "boolean") && user?.role != "agent") {
-        //     this.bot.sendMessage(
-        //       chatId,
-        //       "Оформлять заявки может только агент, обратитесь к администратору /help",
-        //       {}
-        //     );
-        //     return;
-        //   }
-
-        //   // let isCheckUserName = this.checkUserName(chatId, chatUsername);
-        //   // // проверка заполнено ли имя
-        //   // if (!isCheckUserName) {
-        //   //   return;
-        //   // }  
-          
-        //   if((typeof user == "string" || typeof user == "boolean") ) {
-        //     return;
-        //   }
-
-        //   const { spzId, sponsorName, name } = user;          
-
-        //   let { step, message, option } = new SessionOrder(
-        //     msg,
-        //     this.bot
-        //   ).createSession(`${spzId}`, sponsorName, name);
-        //   this.requestMessage(chatId, message, option);
-        //   return;
-        // } else {
-        //   // если что то с таблицами не так, то пользователь получит это сообщение
-        //   this.requestMessage(chatId, `Пока нельзя оформлять заявки, если есть вопросы обратитесь к ${copirite_text.admin}` , {});
-        // }
-      //});
+      await this.bot.sendMessage(chatId, 'Начало оформления заявки', options );  //Начато оформление заявки, заполняйте информацию по очереди
+     new ApiWeb(this.bot).botCommandOrder(chatId);    
     });
   }
 }
